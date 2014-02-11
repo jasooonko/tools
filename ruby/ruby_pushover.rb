72c5b42b8e41c804c75d4c9e9748da3e1b10facd
Jason Ko <jason.ko@mlb.com>
2014-02-11 15:04:02 -0500

    Add newline support for ruby_pushover.rb

--- ruby/ruby_pushover.rb
+++ ruby/ruby_pushover.rb
@@ -3,11 +3,14 @@ require "net/https"
 if ARGV.size != 1
     puts "USAGE: ruby_pushover.rb <message>"
 else
-    message = ARGV[0]
+    
+    message = ARGV[0].gsub("\\n","\n")
+    #puts message.inspect
+    #puts message
     message = message +  "\n ---- \n" +
         "Sent From: " + %x[hostname] +  
         Time.now.to_s + "\n"
-   puts message 
+    puts message 
     url = URI.parse("https://api.pushover.net/1/messages.json")
     req = Net::HTTP::Post.new(url.path)
     req.set_form_data({
