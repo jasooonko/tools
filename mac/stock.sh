e913c12c6164d191a388fa589f619505164fc824
Jason Ko <jason.ko@mlb.com>
2014-03-19 13:46:27 -0400

    add pushover.sh, twiked stock

--- mac/stock.sh
+++
@@ -1,9 +0,0 @@
-stocks=( 'GOOG' 'FB' 'MSFT' 'TSLAT' 'WMT' 'TWTR')
-
-for i in "${stocks[@]}"
-do
-       :
-    echo $i : $`curl -s "http://download.finance.yahoo.com/d/quotes.csv?s=$i&f=l1"`
-
-done
-
